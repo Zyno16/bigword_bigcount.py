89c072b4088afecfdf9eb42da9daf5260ba227e4
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-12 11:35:07 +0000

    Add files via upload

---
+++ bigword_bigcount.py
@@ -0,0 +1,15 @@
+name = input("enter the text")
+handle = open(name)
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word]=counts.get(words , 0) + 1
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount =count
+print(bigword, bigcount)
